71e2a967d613f0597e58857197eae16cfb9916f2
TonyE20 <tonyninjutsu@gmail.com>
2023-02-17 11:34:31 -0800

    proper indivdual func

--- calc/app.py
+++ calc/app.py
@@ -4,6 +4,34 @@ from operations import add, sub, mult, div
 
 app =  Flask(__name__)
 
+@app.route('/add')
+def adding():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    inter_op = add(a, b)
+    return f"<h1>{str(inter_op)}</h1>"
+
+@app.route('/sub')
+def subs():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    inter_op = sub(a, b)
+    return f"<h1>{str(inter_op)}</h1>"
+
+@app.route('/mult')
+def multi():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    inter_op = mult(a, b)
+    return f"<h1>{str(inter_op)}</h1>"
+
+@app.route('/div')
+def division():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    inter_op = div(a, b)
+    return f"<h1>{str(inter_op)}</h1>"
+
 @app.route('/math/<op>')
 def operation(op):
     a = int(request.args["a"])
